d276f4ab88b9c0c4b554319b273facc2b23c5b05
ivanlppires <ivanpires@gmail.com>
2024-10-23 21:55:41 -0400

    CRUD ATUALIZADO COM DELETE E ADIÇÃO

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -2,8 +2,12 @@ import db from '../conf/db.js';
 /* CRUD - CLIENT CONTROLLER */
 const addClient = (req, res) => {
     // Add a new client
+
     const client = req.body;
     const query = `INSERT INTO clients VALUES (uuid(), '${client.name}', '${client.email}')`;
+
+    console.log(query);
+
     db.query(query, (err, result) => {
         if (err) res.status(500);
         res.json(result);
@@ -19,24 +23,26 @@ const getClient = (req, res) => {
     });
 }
 const getClients = (req, res) => {
-    console.log('buscando os clientes no banco')
 
     // Get all clients
     let query = 'SELECT * FROM clients';
+
+    if (req.query.search) {
+        const search = req.query.search;
+        query += ` WHERE name LIKE '%${search}%' OR email LIKE '%${search}%'`
+    }
 
     if (req.query.sortKey && req.query.sortOrder) {
         const orderKey = req.query.sortKey;
         const orderValue = req.query.sortOrder;
         query += ' ORDER BY ' + orderKey + ' ' + orderValue
-    }else{
-        query += ' ORDER BY name asc'        
+    } else {
+        query += ' ORDER BY name asc'
     }
     const page = req.query.page;
     const limit = req.query.limit;
     const offset = (page - 1) * limit;
     query += ' LIMIT ' + limit + ' OFFSET ' + offset
-
-    console.log(query)
 
     db.query(query, (err, result) => {
         if (err) res.status(500);
--- github.sh
+++ github.sh
@@ -1,5 +1,5 @@
 echo "# FMDS_2024-2_AULA-09" >> README.md
 git add .
-git commit -m "CRUD ATUALIZADO PARA FUNCIONAR A LEITURA COM O V-DATA-TABLE-SERVER"
+git commit -m "CRUD ATUALIZADO COM DELETE E ADIÇÃO"
 git branch -M main
 git push -u origin main
